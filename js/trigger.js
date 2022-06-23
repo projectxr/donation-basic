8c289c09bce3ddeace6ac4198aae3dc32f2e3784
Al-amin Sheikh <azalamins@gmail.com>
2022-06-23 18:07:01 +0600

    fixed issue

---
+++ js/trigger.js
@@ -0,0 +1,13 @@
+window.addEventListener("resize", () => {
+  if (window.innerWidth) {
+    window.location.reload();
+  }
+
+  if (window.innerWidth >= 668) {
+    window.location.reload();
+  }
+
+  if (window.innerWidth >= 992) {
+    window.location.reload();
+  }
+});
